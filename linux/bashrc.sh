a3e489241f532402f4c503b9bd728403e849dce9
aQaTL <mmsoltys@outlook.com>
2020-02-07 15:56:16 +0100

    Move local bashrc settings here

--- linux/bashrc.sh
+++ linux/bashrc.sh
@@ -1,3 +1,4 @@
+# git aliases
 alias gs="git status"
 alias ga="git add"
 alias gcm="git commit -m"
@@ -5,3 +6,12 @@ alias gca="git commit -am"
 alias gl="git log --oneline"
 alias gp="git push"
 alias gd="git diff"
+
+alias ls=exa
+alias cat=bat
+alias less=bat
+alias py=python3
+
+PATH="$PATH:$HOME/.local/bin"
+
+PS1="\u|\[\e[33m\]\w\[\e[m\]\\$ "

071a1d3579ba64f3905932b7628df302757a14ef
aQaTL <mmsoltys@outlook.com>
2021-01-15 20:20:29 +0100

    Custom zsh prompt

--- linux/zsh_mac.sh
+++ linux/zsh_mac.sh
@@ -1,7 +1,10 @@
 
 autoload -Uz compinit && compinit
+autoload -U colors && colors
 
 export PATH=$PATH:/opt/homebrew/bin
 export PATH=$PATH:/Applications/MacVim.app/Contents/bin
+
+export PROMPT="%{$fg[cyan]%}%n%{$reset_color%}|%{$fg[yellow]%}%~%{$reset_color%}$ "
 
 eval "$(zoxide init zsh)"

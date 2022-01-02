06f1f614d61f48c8c2dd8974158008c2e9e8d4c3
aQaTL <mmsoltys@outlook.com>
2022-01-02 18:02:09 +0100

    update

--- linux/zsh_mac.sh
+++ linux/zsh_mac.sh
@@ -4,6 +4,7 @@ autoload -U colors && colors
 
 export PATH=$PATH:/opt/homebrew/bin
 export PATH=$PATH:/Applications/MacVim.app/Contents/bin
+export PATH=$PATH:/Users/$USER/jetbrains_scripts
 
 export PROMPT="%{$fg[cyan]%}%n%{$reset_color%}|%{$fg[yellow]%}%~%{$reset_color%}$ "
 

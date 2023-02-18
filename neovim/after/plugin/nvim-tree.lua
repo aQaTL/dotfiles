0ad56fcde3f3c2e3252a265975d097ee2f66c8a5
aQaTL <mmsoltys@outlook.com>
2023-02-18 14:51:11 +0100

    Quit nvim-tree after opening file

--- neovim/after/plugin/nvim-tree.lua
+++ neovim/after/plugin/nvim-tree.lua
@@ -1,4 +1,10 @@
-require("nvim-tree").setup()
+require("nvim-tree").setup {
+	actions = {
+		open_file = {
+			quit_on_open = true
+		}
+	}
+}
 local api = require("nvim-tree.api")
 vim.keymap.set("n", "<leader>e", api.tree.toggle, {})
 vim.keymap.set("n", "<leader>1", api.tree.toggle, {})

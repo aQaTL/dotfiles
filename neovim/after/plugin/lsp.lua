597428aa3275403bf948a6c86df2785fe01d76ea
aQaTL <mmsoltys@outlook.com>
2023-06-24 09:28:34 +0200

    Format on F4

--- neovim/after/plugin/lsp.lua
+++ neovim/after/plugin/lsp.lua
@@ -4,59 +4,60 @@ local telescope = require("telescope.builtin")
 lsp.preset("recommended")
 
 lsp.ensure_installed({
-    'rust_analyzer',
+	'rust_analyzer',
 })
 
 local cmp = require('cmp')
-local cmp_select = {behavior = cmp.SelectBehavior.Select}
+local cmp_select = { behavior = cmp.SelectBehavior.Select }
 local cmp_mappings = lsp.defaults.cmp_mappings({
-    ['<C-p>'] = cmp.mapping.select_prev_item(cmp_select),
-    ['<C-n>'] = cmp.mapping.select_next_item(cmp_select),
-    ['<C-y>'] = cmp.mapping.confirm({ select = true }),
-    ["<C-Space>"] = cmp.mapping.complete(),
+	['<C-p>'] = cmp.mapping.select_prev_item(cmp_select),
+	['<C-n>'] = cmp.mapping.select_next_item(cmp_select),
+	['<C-y>'] = cmp.mapping.confirm({ select = true }),
+	["<C-Space>"] = cmp.mapping.complete(),
 })
 
 lsp.setup_nvim_cmp({
-    mapping = cmp_mappings
+	mapping = cmp_mappings
 })
 
 lsp.on_attach(function(client, bufnr)
-    local opts = {buffer = bufnr, remap = false}
+	local opts = { buffer = bufnr, remap = false }
 
-    if client.name == "eslint" then
-        vim.cmd.LspStop('eslint')
-        return
-    end
+	if client.name == "eslint" then
+		vim.cmd.LspStop('eslint')
+		return
+	end
 
-    vim.api.nvim_create_autocmd({"CursorHold"}, {
-        pattern = {"*.rs"},
-        callback = function(ev)
-            vim.lsp.buf.document_highlight(ev)
-        end
-    })
+	vim.api.nvim_create_autocmd({ "CursorHold" }, {
+		pattern = { "*.rs" },
+		callback = function(ev)
+			vim.lsp.buf.document_highlight(ev)
+		end
+	})
 
-    vim.api.nvim_create_autocmd({"CursorMoved"}, {
-        pattern = {"*.rs"},
-        callback = function(ev)
-            vim.lsp.buf.clear_references()
-        end
-    })
+	vim.api.nvim_create_autocmd({ "CursorMoved" }, {
+		pattern = { "*.rs" },
+		callback = function(ev)
+			vim.lsp.buf.clear_references()
+		end
+	})
 
-    vim.keymap.set("n", "gd", vim.lsp.buf.definition, opts)
-    vim.keymap.set("n", "<F12>", vim.lsp.buf.definition, opts)
-    vim.keymap.set("n", "K", vim.lsp.buf.hover, opts)
-    vim.keymap.set("n", "<leader>vws", vim.lsp.buf.workspace_symbol, opts)
-    vim.keymap.set("n", "<leader>vd", vim.diagnostic.open_float, opts)
-    vim.keymap.set("n", "<F1>", vim.diagnostic.open_float, opts)
-    vim.keymap.set("n", "[d", vim.diagnostic.goto_next, opts)
-    vim.keymap.set("n", "]d", vim.diagnostic.goto_prev, opts)
-    vim.keymap.set("n", "<leader>vca", vim.lsp.buf.code_action, opts)
-    vim.keymap.set("n", "<leader><F1>", vim.lsp.buf.code_action, opts)
-    vim.keymap.set("n", "<leader>vrr", vim.lsp.buf.references, opts)
-    vim.keymap.set("n", "<leader><F7>", vim.lsp.buf.references, opts)
-    vim.keymap.set("n", "<leader>vrn", vim.lsp.buf.rename, opts)
-    vim.keymap.set("i", "<C-h>", vim.lsp.buf.signature_help, opts)
-    vim.keymap.set("n", "<leader><F9>", vim.lsp.buf.document_symbol, opts)
+	vim.keymap.set("n", "gd", vim.lsp.buf.definition, opts)
+	vim.keymap.set("n", "<F12>", vim.lsp.buf.definition, opts)
+	vim.keymap.set("n", "K", vim.lsp.buf.hover, opts)
+	vim.keymap.set("n", "<leader>vws", vim.lsp.buf.workspace_symbol, opts)
+	vim.keymap.set("n", "<leader>vd", vim.diagnostic.open_float, opts)
+	vim.keymap.set("n", "<F1>", vim.diagnostic.open_float, opts)
+	vim.keymap.set("n", "[d", vim.diagnostic.goto_next, opts)
+	vim.keymap.set("n", "]d", vim.diagnostic.goto_prev, opts)
+	vim.keymap.set("n", "<leader>vca", vim.lsp.buf.code_action, opts)
+	vim.keymap.set("n", "<leader><F1>", vim.lsp.buf.code_action, opts)
+	vim.keymap.set("n", "<leader>vrr", vim.lsp.buf.references, opts)
+	vim.keymap.set("n", "<leader><F7>", vim.lsp.buf.references, opts)
+	vim.keymap.set("n", "<leader>vrn", vim.lsp.buf.rename, opts)
+	vim.keymap.set("i", "<C-h>", vim.lsp.buf.signature_help, opts)
+	vim.keymap.set("n", "<leader><F9>", vim.lsp.buf.document_symbol, opts)
+	vim.keymap.set("n", "<F4>", vim.lsp.buf.format, opts)
 
 	-- Telescope pickers
 	vim.keymap.set("n", "<F9>", telescope.lsp_document_symbols, opts)
@@ -65,11 +66,10 @@ lsp.on_attach(function(client, bufnr)
 	vim.keymap.set("n", "<leader>w", telescope.lsp_workspace_symbols, opts)
 
 
-    -- configure document_highlight colors
-    vim.cmd([[ highlight LspReferenceText guibg=#4c4744 ]]) -- slightly brighter than CursorLine
-    vim.cmd([[ highlight link LspReferenceRead CursorLine ]])
-    vim.cmd([[ highlight link LspReferenceWrite CursorLine ]])
+	-- configure document_highlight colors
+	vim.cmd([[ highlight LspReferenceText guibg=#4c4744 ]]) -- slightly brighter than CursorLine
+	vim.cmd([[ highlight link LspReferenceRead CursorLine ]])
+	vim.cmd([[ highlight link LspReferenceWrite CursorLine ]])
 end)
 
 lsp.setup()
-

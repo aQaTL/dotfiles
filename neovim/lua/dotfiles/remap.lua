31b51b4238c5ae288972b0e1c4aed47095a47bd5
aQaTL <mmsoltys@outlook.com>
2023-08-17 21:49:36 +0200

    Fix stupid enter on autocomplete problem

--- neovim/lua/dotfiles/remap.lua
+++ neovim/lua/dotfiles/remap.lua
@@ -29,3 +29,11 @@ vim.keymap.set("n", "<leader>n", function ()
 	vim.cmd([[nohl]])
 end, {})
 
+vim.keymap.set("c", "<cr>", function ()
+	if vim.fn.pumvisible() == 1 then
+		return "c-y"
+	end
+	return "<cr>"
+end,
+{ expr = true }
+)

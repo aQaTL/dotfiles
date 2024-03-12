acb87f19f80f7024c71c1332e276d1b0e401f7dc
aQaTL <mmsoltys@outlook.com>
2024-03-12 12:10:54 +0000

    Add search in current buffer

--- neovim/after/plugin/telescope.lua
+++ neovim/after/plugin/telescope.lua
@@ -3,7 +3,9 @@ vim.keymap.set("n", "<leader>f", builtin.find_files, {})
 vim.keymap.set("n", "<leader>g", builtin.git_files, {})
 vim.keymap.set("n", "<leader>s", builtin.live_grep, {})
 vim.keymap.set("n", "<leader>/", builtin.live_grep, {})
-vim.keymap.set("n", "<F3>", builtin.live_grep, {})
+
+-- TODO: Remove the leader. Problem: lsp-zero default mappings override <F3> when attaching lsp
+vim.keymap.set("n", "<leader><F3>", builtin.current_buffer_fuzzy_find, {})
 
 require("telescope").load_extension "file_browser"
 

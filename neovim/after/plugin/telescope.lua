ff079854d440dd7c34769e831a7e23573f27c67c
aQaTL <mmsoltys@outlook.com>
2024-10-17 10:45:40 +0000

    Add a keymap for finding hidden and gitignored files

--- neovim/after/plugin/telescope.lua
+++ neovim/after/plugin/telescope.lua
@@ -1,5 +1,8 @@
 local builtin = require("telescope.builtin")
 vim.keymap.set("n", "<leader>f", builtin.find_files, {})
+vim.keymap.set("n", "<leader>af", function ()
+	builtin.find_files({ no_ignore = true, hidden = true })
+end, {})
 vim.keymap.set("n", "<leader>g", builtin.git_files, {})
 vim.keymap.set("n", "<leader>s", builtin.live_grep, {})
 vim.keymap.set("n", "<leader>/", builtin.live_grep, {})

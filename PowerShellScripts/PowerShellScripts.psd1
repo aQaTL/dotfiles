33a41b5f378951584123c8f05b362de55c21ea04
aQaTL <mmsoltys@outlook.com>
2025-01-22 20:07:12 +0100

    Add Restart-SshAgentBridge function

--- PowerShellScripts/PowerShellScripts.psd1
+++ PowerShellScripts/PowerShellScripts.psd1
@@ -81,7 +81,8 @@ FunctionsToExport = @(
 	"ConvertFrom-UnixTimeStamp",
 	"ConvertTo-UnixTimeStamp",
 	"Get-GitLargestObjects",
-	"Open-InWindows"
+	"Open-InWindows",
+	"Restart-SshAgentBridge"
 )
 
 # Cmdlets to export from this module, for best performance, do not use wildcards and do not delete the entry, use an empty array if there are no cmdlets to export.
--- PowerShellScripts/wsl.ps1
+++ PowerShellScripts/wsl.ps1
@@ -10,3 +10,22 @@ function Open-InWindows {
 }
 
 Set-Alias -Name openwin -Value Open-InWindows
+
+function Restart-SshAgentBridge {
+	if (-not (Test-Path Env:\SSH_AUTH_SOCK)) {
+		throw "SSH_AUTH_SOCK environment variable not set"
+	}
+
+	if (Test-Path $env:SSH_AUTH_SOCK) {
+		Write-Host "Removing socket (${env:SSH_AUTH_SOCK})"
+		Remove-Item $env:SSH_AUTH_SOCK
+	}
+
+	if ($null -ne (Get-Process | Where-Object Name -eq "socat" -OutVariable socat_process)) {
+		Write-Host "Kiling socat"
+		$socat_process | Stop-Process
+	}
+
+	Write-Host "starting socat"
+	setsid socat "UNIX-LISTEN:${env:SSH_AUTH_SOCK},fork" "EXEC:`"npiperelay.exe -ei -s //./pipe/openssh-ssh-agent`",nofork" &
+}
